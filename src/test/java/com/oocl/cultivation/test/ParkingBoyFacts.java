88d13bc5af69c984651419d1c465cfbddecb9a0c
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-13 17:39:01 +0800

    Another test for story 2

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -72,16 +72,16 @@ class ParkingBoyFacts {
 
     @Test
         void should_return_no_car_when_ticket_is_used() {
-            // given;
-            Car car1 = new Car();
+        // given;
+        Car car1 = new Car();
 
-            ParkingLot parkingLot = new ParkingLot();
-            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
 
-            // when
-            ParkingTicket parkingTicket1 = parkingBoy.park(car1);
-            Car carGet = parkingBoy.fetch(parkingTicket1);
-            Car carGet2 = parkingBoy.fetch(parkingTicket1);
+        // when
+        ParkingTicket parkingTicket1 = parkingBoy.park(car1);
+        Car carGet = parkingBoy.fetch(parkingTicket1);
+        Car carGet2 = parkingBoy.fetch(parkingTicket1);
 
         // then
         assertNotNull(carGet);
@@ -120,5 +120,19 @@ class ParkingBoyFacts {
         // then
         assertNull(carGet);
         assertEquals(parkingBoy.getLastErrorMessage(),"Please provide your parking ticket.");
+    }
+
+    @Test
+    void should_get_error_message_when_wrong_ticket_is_given() {
+        // given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        // when
+        Car carGet = parkingBoy.fetch(new ParkingTicket());
+
+        // then
+        assertNull(carGet);
+        assertEquals(parkingBoy.getLastErrorMessage(),"Unrecognized parking ticket");
     }
 }
